b9bac6fa5dbfd638f24e641479baec9da157b17f
bellawn99 <bellawulan99@mail.ugm.ac.id>
2019-05-25 09:46:53 +0700

    Created Model

---
+++ app/Http/Controllers/BungaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Bunga;
+use Illuminate\Http\Request;
+
+class BungaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Bunga  $bunga
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bunga $bunga)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Bunga  $bunga
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bunga $bunga)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Bunga  $bunga
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bunga $bunga)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Bunga  $bunga
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bunga $bunga)
+    {
+        //
+    }
+}
---
+++ database/factories/BungaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Bunga;
+use Faker\Generator as Faker;
+
+$factory->define(Bunga::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
